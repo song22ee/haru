3c0858a2d24597cac74a2036d3ad895ff526097d
song22ee <ann7335@naver.com>
2022-11-10 17:05:21 +0900

    footer 분리해서 저장

--- index.php
+++ index.php
@@ -248,59 +248,7 @@
         </div>
     </main>
 
-    <footer>
-        <div class="footer_nav_wrapper">
-            <div class="customer_center_wrapper">
-                <span class="customer_center_title">Customer Center</span>
-                <button class="question_kakao">카카오톡 문의</button>
-                <button class="return_fast">빠른 교환/반품 신청</button>
-                <div class="haru_call_wrapper">
-                    <i class="fas fa-phone-alt"></i>
-                    <span>1588-1234</span>
-                </div>
-            </div>
-            <div class="haru_operating_time_wrapper">
-                <span class="haru_operating_time_title">Time</span>
-                <span class="call_time_title">전화 상담</span>
-                <p class="call_time">평일 : 10:00 ~ 18:00 <br />
-                    토요일 : 10:00 ~ 15:00 </p>
-                <span class="katalk_time_title">카톡 상담</span>
-                <p class="katalk_time">평일 : 10:00 ~ 18:00 <br />
-                    토요일 : 10:00 ~ 15:00</p>
-            </div>
-            <div class="notice_wrapper">
-                <span class="notice_title">Notice</span>
-                <div class="notice_container"></div>
-            </div>
-        </div>
-        <div class="footer_nav_wrapper">
-            <div class="guide_wrapper">
-                <span class="guide_title">GUIDE</span>
-                <a href="" class="terms_service">이용약관</a>
-                <a href="" class="privacy_policy">개인정보 처리방침</a>
-
-            </div>
-            <div class="about_us_wrapper">
-                <span class="about_us_title">ABOUT US</span>
-                <a href="" class="membership_info">멤버십 안내</a>
-            </div>
-        </div>
-        <div class="footer_haru_info">
-            <span class="about_haru_title">ABOUT SIMPLE HARU</span>
-            <ul>
-                <li>상호 : 심플 하루 그룹(주)</li>
-                <li>
-                    <address>사업장 소재지 : 경기도 성남시 중원구 광명로 377 </address>
-                </li>
-                <li>사업자 등록 번호 : 123456-1245678</li>
-                <li>대표 이사 : 정수하 송가현</li>
-                <li>대표 전화 : 1588-1234</li>
-            </ul>
-            <ul>
-                <li>COPYRIGHT(c) 2022 HARU ALL RIGHT RESERVED.</li>
-            </ul>
-        </div>
-    </footer>
+    <?php require_once("inc/footer.php"); ?>
 
     <script src="https://kit.fontawesome.com/73fbcb87e6.js" crossorigin="anonymous"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js"></script>

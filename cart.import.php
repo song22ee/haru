9c48a552f092180267b3f8a883736f08c98da854
song22ee <ann7335@naver.com>
2022-11-17 01:54:28 +0900

    branch 테스트

--- cart.import.php
+++ cart.import.php
@@ -1,7 +1,7 @@
 <?php
 
 require_once("inc/db.php");
-
+require_once("inc/db.php");
 
     //로그인 되어 있는 사용자의 아이디 가져오기
     if (isset($_SESSION) === false){session_start();}
@@ -11,7 +11,7 @@ var_dump($user_id);
 
 
 $result =db_select("select * from cart");
-
+fsfsd
 
 
 // print_r($result);    

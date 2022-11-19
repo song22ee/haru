57786a480c8e00bb9afbf00a5257329574bd3668
song22ee <ann7335@naver.com>
2022-11-19 12:32:04 +0900

    test1

--- contents_insert.php
+++ contents_insert.php
@@ -35,17 +35,55 @@ $content_cost=$_POST["product_cost"];
 // var_dump($content_img3);
 // var_dump($content_img4);
 
+//카페고리 대분류 한글로 변환하기
+
+
+switch($category_large){
+    case 'outer' :
+        $category_large='아우터';
+        break;
+    case 'top' :
+        $category_large='상의';
+        break;
+    case 'traning' :
+        $category_large='트레이닝';
+        break;
+    case 'basic' :
+        $category_large='베이직';
+        break;
+    case 'one_piece' :
+        $category_large='원피스';
+        break;
+    case 'skirt' :
+        $category_large='스커트';
+        break;
+    case 'pants' :
+        $category_large='하의';
+        break;
+    case 'bag' :
+        $category_large='가방';
+        break;
+    case 'shoes' :
+        $category_large='신발';
+        break;
+    case 'accessory' :
+        $category_large='액세서리';
+        break;
+
+}
+
+
 require_once("inc/db.php");
 
 // 데이터 저장
     db_insert("insert into contents 
     (content_code , content_img,deliv_today,content_name,
-    discount_rate,content_price,content_color1,content_color2,content_color3,content_color4,
-    category_large,category_small, content_img1, content_img2, content_img3, content_img4,content_cost) 
+    discount_rate,content_cost,content_price,content_color1,content_color2,content_color3,content_color4,
+    category_large,category_small, content_img1, content_img2, content_img3, content_img4) 
     values 
     (:content_code , :content_img,:deliv_today,:content_name,
-    :discount_rate,:content_price,:content_color1,:content_color2,:content_color3,:content_color4,
-    :category_large, :category_small, :content_img1, :content_img2, :content_img3, :content_img4,content_cost )",
+    :discount_rate,,:content_cost,:content_price,:content_color1,:content_color2,:content_color3,:content_color4,
+    :category_large, :category_small, :content_img1, :content_img2, :content_img3, :content_img4 )",
         array(
             'content_code' => $content_code,
             'content_img' => $content_img,
